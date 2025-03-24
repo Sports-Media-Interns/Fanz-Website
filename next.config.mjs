3ec634c74ddc3bb2f038eb4ed564181efd3de399
VyshnaviMulakalapalli <vm1096@usnh.edu>
2025-03-24 12:33:59 -0400

    update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -20,9 +20,31 @@ const nextConfig = {
         protocol: "https",
         hostname: "firebasestorage.googleapis.com",
       },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
     ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `node:` protocol
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        path: false,
+        os: false,
+        http: false,
+        https: false,
+        stream: false,
+      };
+    }
+    return config;
   },
   async rewrites() {
     return [
